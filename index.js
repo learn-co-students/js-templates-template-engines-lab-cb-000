38ab9ebf5bb6ab6a939f57a2c5304fc479f7f889
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-04-12 21:46:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function postComment(){
 
+  let commenter = document.getElementById('commenterName').value;
+  let comment = document.getElementById('commentText').value;
+
+  let commentTemplate = document.getElementById('comment-template').innerHTML;
+  let templateFn = _.template(commentTemplate);
+  let commentsDiv = document.getElementById('comments');
+
+  //execute template function with JSON object for the interpolated values
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+
 }
 
 function createPost(){
